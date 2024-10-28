531771334c3fafa618c60ce36ca33ea2157371fc
aalbertvilarnau <avilarnauillana@gmail.com>
2024-10-28 13:15:50 +0100

    Fix

--- JAVASCRIPT/script_login.js
+++ JAVASCRIPT/script_login.js
@@ -1,52 +1,60 @@
-let usuarios = {
-    "user0": {user:"admin", password:"1111", q1:0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0, q8:0, active:false},
-    "user1": {user:"albert", password:"1111", q1:0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0, q8:0, active:false},
-    "user2": {user:"kike", password:"1111", q1:0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0, q8:0, active:false},
-}
+function LogIn() {
+    let usuarios = [
+        ["admin", "1111", 0,0,0,0,0,0,0,0,false],
+        ["albert", "1111", 0,0,0,0,0,0,0,0,false],
+        ["kike", "1111", 0,0,0,0,0,0,0,0,false],
+    ];
+    
+    if (!localStorage.getItem("usuarios")) {
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    }
 
-if (!localStorage.getItem("usuarios")) {
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
-}
-function LogIn(){
     document.getElementById("error_password").innerHTML = "";
     document.getElementById("error_user").innerHTML = "";
     document.getElementById("error_general").innerHTML = "";
 
-    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
-    let ui_user = document.getElementById("ui_user").value.trim();
-    let ui_password = document.getElementById("ui_password").value.trim();
+    usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    let ui_user = document.getElementById("ui_user").value;
+    let ui_password = document.getElementById("ui_password").value;
 
-    for(let i = 0; i < Object.keys(usuarios).length; i++){
-         if(!ui_password){
-            document.getElementById("error_password").innerHTML = "Password is required"
-        } if(!ui_user){
-            document.getElementById("error_user").innerHTML = "User is required"
-        } if( (ui_user && ui_password) && ui_user != usuarios[`user${i}`].user || ui_password != usuarios[`user${i}`].password){
-            document.getElementById("error_general").innerHTML = "User or password is incorrect"
-        } if(ui_user == usuarios[`user${i}`].user && ui_password == usuarios[`user${i}`].password){
-            usuarios[`user${i}`].active = true;
-            alert(`Welcome, ${usuarios[`user${i}`].user} to the Sugar Cave`);
-            let usuariosString = JSON.stringify(usuarios);
-            localStorage.setItem("usuarios", usuariosString);
-            console.log(usuarios[`user${i}`]);
+    for (let i = 0; i < usuarios.length; i++) {
+        if (!ui_password) {
+            document.getElementById("error_password").innerHTML = "Password is required";
+        }
+        if (!ui_user) {
+            document.getElementById("error_user").innerHTML = "User is required";
+        }
+        if ((ui_user && ui_password) && (ui_user != usuarios[i][0] || ui_password != usuarios[i][1])) {
+            document.getElementById("error_general").innerHTML = "User or password is incorrect";
+        }
+        if (ui_user == usuarios[i][0] && ui_password == usuarios[i][1]) {
+            usuarios[i][10] = true;
+            alert(`Welcome, ${usuarios[i][0]} to the Sugar Cave`);
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
             location.href = "store.html";
-
+            break;
         } 
     }
 }
 
 window.onload = function CheckUsers() {
-    usuariosString = localStorage.getItem("usuarios");
-    usuarios = JSON.parse(usuariosString);
+    let usuariosString = localStorage.getItem("usuarios");
+    let usuarios = JSON.parse(usuariosString);
 
-    for (let i = 0; i < Object.keys(usuarios).length; i++) {
-        if (usuarios[`user${i}`].active) {
-            alert(`Welcome, ${usuarios[`user${i}`].user} to the Sugar Cave`);
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i][10]) {
+            alert(`Welcome, ${usuarios[i][0]} to the Sugar Cave`);
             location.href = "store.html";
             break;
         }
     }
 }
-function ClearCache(){
-    localStorage.clear();
+
+function ClearCache() {
+    let usuarios = [
+        ["admin", "1111", 0,0,0,0,0,0,0,0,false],
+        ["albert", "1111", 0,0,0,0,0,0,0,0,false],
+        ["kike", "1111", 0,0,0,0,0,0,0,0,false],
+    ];
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
 }
