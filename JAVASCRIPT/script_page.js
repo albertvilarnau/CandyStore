0cc7f8b4fb56542e51df5b92a227f7afb50b6309
Albert Vilarnau <avilarnauillana@gmail.com>
2024-10-28 19:16:28 +0100

    Fix

--- JAVASCRIPT/script_page.js
+++ JAVASCRIPT/script_page.js
@@ -1,48 +1,55 @@
+function LogOut() {
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    if (!usuarios) return;
 
-function LogOut(){
-    usuarios = JSON.parse(localStorage.getItem("usuarios"));
-    for(let i = 0; i < usuarios.length; i++){
-        if (usuarios[i][10] = true){
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i][10] === true) {
             usuarios[i][10] = false;
         }
-        localStorage.setItem("usuarios", JSON.stringify(usuarios));
-        location.href = "index.html"
     }
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    location.href = "index.html";
 }
 
-window.onload = function OnLoadPage(){
+window.onload = function OnLoadPage() {
     let ActiveUser = "";
-    usuarios = JSON.parse(localStorage.getItem("usuarios"));
-    for(let i = 0; i < usuarios.length; i++){
-        if (usuarios[i][10] == true){
-            ActiveUser = usuarios[i][0];
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    if (usuarios) {
+        for (let i = 0; i < usuarios.length; i++) {
+            if (usuarios[i][10] === true) {
+                ActiveUser = usuarios[i][0];
+            }
         }
     }
     document.getElementById("user_header").innerHTML = ActiveUser;
 
-    if (!localStorage.getItem("cesta")) {
-        let cesta = [0,0,0,0,0,0,0,0,];
-        localStorage.setItem("cesta", JSON.stringify(cesta));
-    }
-    
+    initializeCesta(); // Inicializa la cesta
+
     let cesta = JSON.parse(localStorage.getItem("cesta"));
-    for(let i = 0; i < cesta.length; i++){
-        document.getElementById(`cesta_span${i}`).innerHTML = cesta[i];
+    if (cesta) {
+        for (let i = 0; i < cesta.length; i++) {
+            document.getElementById(`cesta_span${i}`).innerHTML = cesta[i];
+        }
     }
 }
 
-function Redirect(site){
-    location.href = site
-}
-
-function AddCesta(num){
+function initializeCesta() {
     if (!localStorage.getItem("cesta")) {
-        let cesta = [0,0,0,0,0,0,0,0,];
+        let cesta = [0, 0, 0, 0, 0, 0, 0, 0];
         localStorage.setItem("cesta", JSON.stringify(cesta));
     }
+}
 
+function Redirect(site) {
+    location.href = site;
+}
+
+function ModCesta(producto, cantidad) {
+    initializeCesta();
     let cesta = JSON.parse(localStorage.getItem("cesta"));
-    cesta[num]++;
-    document.getElementById(`cesta_span${num}`).innerHTML = cesta[num];
-    localStorage.setItem("cesta", JSON.stringify(cesta))
+    if((cantidad == -1 && cesta[producto] > 0)||(cantidad == 1 && cesta[producto] >= 0)){
+        cesta[producto] += cantidad;
+        document.getElementById(`cesta_span${producto}`).innerHTML = cesta[producto];
+        localStorage.setItem("cesta", JSON.stringify(cesta));
+    }
 }
