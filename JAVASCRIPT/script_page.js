4704ecb432a3c8afd81321ede0fc02dac456c42e
Albert Vilarnau <avilarnauillana@gmail.com>
2024-10-29 18:28:13 +0100

    Fix

--- JAVASCRIPT/script_page.js
+++ JAVASCRIPT/script_page.js
@@ -12,24 +12,11 @@ function LogOut() {
 }
 
 window.onload = function OnLoadPage() {
-    let ActiveUser = "";
-    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
-    if (usuarios) {
-        for (let i = 0; i < usuarios.length; i++) {
-            if (usuarios[i][4]) {
-                ActiveUser = usuarios[i][0];
-                for (let n = 0; n < usuarios[i][3].length; n++) {
-                    document.getElementById(`cesta_span${n}`).innerHTML = usuarios[i][3][n];
-                }
-            }
-        }
-    }
+    let ActiveUser = GetActiveUser();
+    console.log(ActiveUser);
+    SetAllCesta_Span();
     document.getElementById("user_header").innerHTML = ActiveUser;
 };
-
-function Redirect(site) {
-    location.href = site;
-}
 
 function ModCesta(producto, cantidad) {
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
@@ -42,12 +29,11 @@ function ModCesta(producto, cantidad) {
                 (cantidad === 1 && usuarios[i][3][producto] >= 0)
             ) {
                 usuarios[i][3][producto] += cantidad;
-                document.getElementById(`cesta_span${producto}`).innerHTML = usuarios[i][3][producto];
                 localStorage.setItem("usuarios", JSON.stringify(usuarios));
             }
-            break;
         }
     }
+    SetAllCesta_Span();
 }
 
 function CompletarCompra() {
@@ -74,10 +60,49 @@ function VaciarCesta() {
         if (usuarios[i][4]) {
             for (let n = 0; n < usuarios[i][3].length; n++) {
                 usuarios[i][3][n] = 0;
-                document.getElementById(`cesta_span${n}`).innerHTML = usuarios[i][3][n];
             }
-            break;
         }
     }
     localStorage.setItem("usuarios", JSON.stringify(usuarios)); 
+    SetAllCesta_Span();
+}
+
+function GetActiveUser(){
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    if (usuarios) {
+        for (let i = 0; i < usuarios.length; i++) {
+            if (usuarios[i][4]) {
+                return usuarios[i][0];
+            }
+        }
+    }
+}
+
+function SetAllCesta_Span() {
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    let total_cesta = 0;
+    if (usuarios) {
+        for (let i = 0; i < usuarios.length; i++) {
+            if (usuarios[i][4]) { 
+                for (let n = 0; n < usuarios[i][3].length; n++) {
+                    let cantidad = usuarios[i][3][n];
+                    let precio = cantidad * 0.5;
+                    total_cesta += cantidad;
+                    if (window.location.pathname.includes("store.html")) {
+                        document.getElementById(`cesta_span${n}`).innerHTML = cantidad;
+                    }
+
+                    if (window.location.pathname.includes("cesta.html")) {
+                            document.getElementById(`price_span${n}`).innerHTML = precio.toFixed(2);
+                            document.getElementById(`cesta_span${n}`).innerHTML = cantidad;
+                    }
+                    let elementos = document.getElementsByClassName(`cesta_producto${n}`);
+                    for (let elem of elementos) {
+                        elem.style.display = (cantidad > 0) ? 'flex' : 'none';
+                    }
+                }
+            }
+        }
+    }
+    document.getElementById("total_cart").innerHTML = total_cesta;
 }
