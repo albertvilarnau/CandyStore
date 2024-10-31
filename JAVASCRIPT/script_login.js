96a2416bf3c3cc0fc7025d85bf10f6921f2572e8
aalbertvilarnau <avilarnauillana@gmail.com>
2024-10-31 09:18:02 +0100

    Fixfinal

--- JAVASCRIPT/script_login.js
+++ JAVASCRIPT/script_login.js
@@ -1,36 +1,44 @@
-// Script 1: login
 function LogIn() {
+    // Primero, si el local storage no existe, lo creamos y definimos un usuario base, que es ADMIN
     if (!localStorage.getItem("usuarios")) {
         let usuarios = [
             ["admin", "1111", [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], false, 1],
-            ["albert", "1111", [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], false, 2],
-            ["kike", "1111", [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], false, 3],
         ];
+        // Una vez definido el array de usuarios, lo guardamos en el local storage. 
+        // Ya que solo se puede guardar un valor en el local storage, lo convertimos a string con JSON.stringify
         localStorage.setItem("usuarios", JSON.stringify(usuarios));
     }
 
+    // Reiniciamos los usuarios 
     document.getElementById("error_password").innerHTML = "";
     document.getElementById("error_user").innerHTML = "";
     document.getElementById("error_general").innerHTML = "";
 
+    // Declaramos los usuarios y el input de lusuario
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
     let ui_user = document.getElementById("ui_user").value;
     let ui_password = document.getElementById("ui_password").value;
 
+    // Errores por si el input es vacio
     if (!ui_user) {
         document.getElementById("error_user").innerHTML = "User is required";
-        return;
     }
     if (!ui_password) {
         document.getElementById("error_password").innerHTML = "Password is required";
+    }
+    if (!ui_user || !ui_password) {
         return;
     }
-
+    
+    // Funcion que recorre todos los usuarios y los compara con nuestro input
     let validUser = false;
     for (let i = 0; i < usuarios.length; i++) {
         if (ui_user === usuarios[i][0] && ui_password === usuarios[i][1]) {
+            //  Si el usuario y contraseña son correctos, validamos el usuario y le ponemos [i][4] 
+            // como true, que es si el usuario esta validado en ese momento
             usuarios[i][4] = true;
             alert(`Welcome, ${usuarios[i][0]} to the Sugar Cave`);
+            // Devolvemos el array de usuarios  actualizado y redirigimos a la store
             localStorage.setItem("usuarios", JSON.stringify(usuarios));
             location.href = "store.html";
             validUser = true;
@@ -38,11 +46,13 @@ function LogIn() {
         }
     }
 
+    // Si no se valida el user, mostramos el error
     if (!validUser) {
         document.getElementById("error_general").innerHTML = "User or password is incorrect";
     }
 }
 
+// Al cargar la pagina de login, si hay un usuario activo, por lo tanto, esta logueado, redirigimos a la store
 window.onload = function CheckUsers() {
     let usuariosString = localStorage.getItem("usuarios");
     if (!usuariosString) return;
@@ -57,11 +67,37 @@ window.onload = function CheckUsers() {
     }
 };
 
+// Usamos una funcion de Clean Cache que se usa para predefinir el valor del local storage, usado en el desarrollo de la web
 function ClearCache() {
     let usuarios = [
         ["admin", "1111", [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], false, 1],
-        ["albert", "1111", [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], false, 2],
-        ["kike", "1111", [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], false, 3],
+
     ];
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
+}
+
+// Funcion que añade un usuario, creando un array con los datos que introduce,  y lo añade al array de usuarios
+function AddUser() {
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    let ui_user = document.getElementById("ui_user").value;
+    let ui_password = document.getElementById("ui_password").value;
+    
+    if (!ui_user) {
+        document.getElementById("error_user").innerHTML = "User is required";
+        return;
+    }
+    if (!ui_password) {
+        document.getElementById("error_password").innerHTML = "Password is required";
+        return;
+    }
+    // Creamos el array  con los datos del usuario y lo asignamos  a la variable usuarios
+
+    newUsuario = [ui_user, ui_password, [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], false, (usuarios.length+1)],
+
+    usuarios.push(newUsuario);
+
+    alert("Usuario creado");
+    // Devolvemos usuarios actualizado
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
 }
--- JAVASCRIPT/script_page.js
+++ JAVASCRIPT/script_page.js
@@ -1,35 +1,44 @@
 function LogOut() {
+    // Funcion para hacer log out
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
     if (!usuarios) return;
-
+    // El usuario actual lo volvemos active = false cosa que hace el logout
     for (let i = 0; i < usuarios.length; i++) {
         if (usuarios[i][4]) {
             usuarios[i][4] = false;
         }
     }
+    //  Guardamos los cambios en el localStorage
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
     location.href = "index.html";
 }
 
+// Al cargar la pagina
 window.onload = function OnLoadPage() {
-    let ActiveUser = GetActiveUser();
-    console.log(ActiveUser);
     SetAllCesta_Span();
-    document.getElementById("user_header").innerHTML = ActiveUser;
-
-
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    if (usuarios) {
+        for (let i = 0; i < usuarios.length; i++) {
+            if (usuarios[i][4]) {
+                document.getElementById("user_header").innerHTML = usuarios[i][0];
+            }
+        }
+    }    // Ponemos el usuario en el header
+    ValidarUser();
 
 };
 
+// Con esta funcion modificamos cesta. tiene dos parametros, el producto, que es la "ID" del producto, y la cantidad, siempre +1 o -1
 function ModCesta(producto, cantidad) {
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
     if (!usuarios) return;
 
+    // Logica que recorre los usuarios y resta o suma a la cesta, y se asegura que no baje nunca de 0 la cesta, luego actualizamos todos los span   
     for (let i = 0; i < usuarios.length; i++) {
         if (usuarios[i][4]) {
             if (
-                (cantidad === -1 && usuarios[i][3][producto] > 0) ||
-                (cantidad === 1 && usuarios[i][3][producto] >= 0)
+                (cantidad == -1 && usuarios[i][3][producto] > 0) ||
+                (cantidad == 1 && usuarios[i][3][producto] >= 0)
             ) {
                 usuarios[i][3][producto] += cantidad;
                 localStorage.setItem("usuarios", JSON.stringify(usuarios));
@@ -42,14 +51,16 @@ function ModCesta(producto, cantidad) {
 function CompletarCompra() {
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
     if (!usuarios) return;
-
+    // Para completar la compra, sumamos todos los  productos de la cesta del usuario al array de compra confirmada (2)
     for (let i = 0; i < usuarios.length; i++) {
         if (usuarios[i][4]) {
             for (let n = 0; n < usuarios[i][3].length; n++) {
                 usuarios[i][2][n] += usuarios[i][3][n]; 
             }
+            // Actualizamos local storage y vaciamos la cesta
             localStorage.setItem("usuarios", JSON.stringify(usuarios)); 
             VaciarCesta();
+            alert("Compra completada")
             break;
         }
     }
@@ -58,7 +69,7 @@ function CompletarCompra() {
 function VaciarCesta() {
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
     if (!usuarios) return;
-
+    // Vaciamos la cesta, llevandolo todo a 0 del user activo
     for (let i = 0; i < usuarios.length; i++) {
         if (usuarios[i][4]) {
             for (let n = 0; n < usuarios[i][3].length; n++) {
@@ -67,50 +78,74 @@ function VaciarCesta() {
         }
     }
     localStorage.setItem("usuarios", JSON.stringify(usuarios)); 
+    //  Luego actualizamos todos los span
     SetAllCesta_Span();
 }
 
-function GetActiveUser(){
-    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
-    if (usuarios) {
-        for (let i = 0; i < usuarios.length; i++) {
-            if (usuarios[i][4]) {
-                return usuarios[i][0];
-            }
-        }
-    }
-}
-
 function SetAllCesta_Span() {
+    // Recupera la lista de usuarios almacenada en localStorage y la convierte de JSON a un objeto JavaScript
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    // Inicializa una variable para mantener el total de la cesta
     let total_cesta = 0;
+
+    // Verifica si existen usuarios en el localStorage
     if (usuarios) {
+        // Itera sobre cada usuario
         for (let i = 0; i < usuarios.length; i++) {
+            // Comprueba si el usuario tiene un valor verdadero en el 4, que indica que esta activo
             if (usuarios[i][4]) { 
+                // Itera sobre los productos del usuario
+                let precio_total_cesta = 0;
                 for (let n = 0; n < usuarios[i][3].length; n++) {
+                    // Obtiene la cantidad de productos y la cantidad de compra
                     let cantidad = usuarios[i][3][n];
                     let cantidad_compra = usuarios[i][2][n];
 
-                    let precio = cantidad * 0.5;
+                    // Calcula el precio del producto (asumiendo un precio fijo de 0.5 por unidad)
+                    let precio = 0;
+                    // Suma la cantidad total a la cesta
                     total_cesta += cantidad;
+                    
+
+                    if(n == 0 || n == 1){
+                        precio = cantidad * 0.5
+                    } else if(n == 2 || n == 3){
+                        precio = cantidad * 0.99;
+                    } else if(n == 4){
+                        precio = cantidad * 2.20;
+                    } else if(n == 5){
+                        precio = cantidad * 6.99;
+                    } else if(n == 6 || n == 7){
+                        precio = cantidad * 4.99;
+                    }
+
+                    precio_total_cesta += precio;
+
+                    // Actualiza el contenido del HTML dependiendo de la página actual
                     if (window.location.pathname.includes("store.html")) {
+                        // Si estamos en la página de tienda, muestra la cantidad en el span correspondiente
                         document.getElementById(`cesta_span${n}`).innerHTML = cantidad;
                     }
 
                     if (window.location.pathname.includes("cesta.html")) {
-                            document.getElementById(`price_span${n}`).innerHTML = precio.toFixed(2);
-                            document.getElementById(`cesta_span${n}`).innerHTML = cantidad;
+                        // Si estamos en la página de la cesta, muestra el precio y la cantidad
+                        document.getElementById(`price_span${n}`).innerHTML = precio.toFixed(2);
+                        document.getElementById(`cesta_span${n}`).innerHTML = cantidad;
+                        document.getElementById("precio_total_cesta").innerHTML = precio_total_cesta.toFixed(2);
                     }
 
                     if (window.location.pathname.includes("user.html")) {
+                        // Si estamos en la página de usuario, muestra la cantidad de compra
                         document.getElementById(`cantidad_compra${n}`).innerHTML = cantidad_compra;
                     }
 
+                    // Muestra u oculta los elementos de la cesta según la cantidad de productos
                     let elementos = document.getElementsByClassName(`cesta_producto${n}`);
                     for (let elem of elementos) {
                         elem.style.display = (cantidad > 0) ? 'flex' : 'none';
                     }
 
+                    // Muestra u oculta los elementos de usuario según la cantidad de compra
                     let elementos2 = document.getElementsByClassName(`product_user${n}`);
                     for (let elem of elementos2) {
                         elem.style.display = (cantidad_compra > 0) ? 'flex' : 'none';
@@ -119,7 +154,30 @@ function SetAllCesta_Span() {
             }
         }
     }
+    // Actualiza el total de la cesta en el HTML
     document.getElementById("total_cart").innerHTML = total_cesta;
 }
 
+function ValidarUser () {
+    // Recupera la lista de usuarios almacenada en localStorage
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    // Inicializa un contador para contar usuarios activos
+    let contador = 0;
 
+    // Verifica si existen usuarios en el localStorage
+    if (usuarios) {
+        // Itera sobre cada usuario
+        for (let i = 0; i < usuarios.length; i++) {
+            // Comprueba si el usuario tiene un valor verdadero en la posición 4 del array
+            if (usuarios[i][4]) { 
+                contador++; // Incrementa el contador si el usuario está activo
+            }
+        }
+    }
+    
+    // Si no hay usuarios activos, muestra una alerta y redirige a la página principal
+    if (contador == 0) {
+        alert("No estas logueado");
+        location.href = "index.html";
+    }
+}
